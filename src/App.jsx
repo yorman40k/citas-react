790f6ffcdc0585e6da3e105929cfc7ccd8c1713c
yorman <yorman.arango@sofka.com.co>
2023-03-20 14:51:52 -0500

    wip: create two new components and they have been added to App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Formulario from "./components/Formulario"
 import Header from "./components/Header"
+import PacientsList from "./components/PacientsList"
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Formulario/>
+      <PacientsList/>
     </>
   )
 }
---
+++ src/components/PacientsList.jsx
@@ -0,0 +1,9 @@
+function PacientsList() {
+  return (
+    <>
+        <h2>listado de pacientes</h2>
+    </>
+  )
+}
+
+export default PacientsList
