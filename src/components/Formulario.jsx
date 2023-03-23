e2fb2ae48970b113742c851f5bb7ff73d6331dc8
yorman <yorman.arango@sofka.com.co>
2023-03-23 16:03:52 -0500

    wip: use the hook useState in every form input

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,12 +2,22 @@ import { useState, useEffect } from 'react'
 
 const Formulario = () => {
 
-  const [nombre, setNombre] = useState('');
+  const [mascota, setMascota] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
 
-  console.log(nombre);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Enviando formulario');
+  }
+
+  console.log(mascota);
 
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
         <h2 className="font-black text-3xl text-center">
           Seguimiento Pacientes
         </h2>
@@ -19,7 +29,10 @@ const Formulario = () => {
           </span>
         </p>
 
-        <form className="bg-white shadow-md rounded-lg ml-10 py-10 px-5 mb-20">
+        <form 
+          onSubmit={handleSubmit}
+          className="bg-white shadow-md rounded-lg ml-10 py-10 px-5 mb-20"
+        >
           <div className="mb-5">
             <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
               Nombre mascota
@@ -30,8 +43,8 @@ const Formulario = () => {
               type="text"
               placeholder="Nombre de la mascota"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
-              value={nombre}
-              onChange={(e) => setNombre(e.target.value)}
+              value={mascota}
+              onChange={(e) => setMascota(e.target.value)}
             />
           </div>
 
@@ -45,6 +58,8 @@ const Formulario = () => {
               type="text"
               placeholder="Nombre del propietario"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+              value={propietario}
+              onChange={(e) => setPropietario(e.target.value)}
             />
           </div>
 
@@ -58,6 +73,8 @@ const Formulario = () => {
               type="email"
               placeholder="Email"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
 
@@ -70,6 +87,8 @@ const Formulario = () => {
               id="alta"
               type="date"
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" 
+              value={fecha}
+              onChange={(e) => setFecha(e.target.value)}
             />
           </div>
 
@@ -82,6 +101,8 @@ const Formulario = () => {
               id="sintomas" 
               className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
               placeholder="Describe los síntomas"
+              value={sintomas}
+              onChange={(e) => setSintomas(e.target.value)}
             />
           </div>
 
--- src/components/Pacient.jsx
+++ src/components/Pacient.jsx
@@ -1,24 +1,24 @@
 const Pacient = () => {
   return (
-    <div className="m-10 bg-white shadow-md px-5 py-10 rounded-xl">
+    <div className="mx-5 my-8 bg-white shadow-md px-5 py-10 rounded-xl">
         <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""}
-            <span className="font-normal normal">Hook</span>
+            <span className="font-normal normal-case">Hook</span>
           </p>
 
           <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {""}
-            <span className="font-normal normal">Juanito Alimaña</span>
+            <span className="font-normal normal-case">Juanito Alimaña</span>
           </p>
 
           <p className="font-bold mb-3 text-gray-700 uppercase">Email: {""}
-            <span className="font-normal normal">correo@correo.com</span>
+            <span className="font-normal normal-case">correo@correo.com</span>
           </p>
 
           <p className="font-bold mb-3 text-gray-700 uppercase">Fecha de alta: {""}
-            <span className="font-normal normal">10 de Diciembre de 2022</span>
+            <span className="font-normal normal-case">10 de Diciembre de 2022</span>
           </p>
 
           <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {""}
-            <span className="font-normal normal">Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <span className="font-normal normal-case">Lorem ipsum dolor sit amet consectetur adipisicing elit.
              Ab nobis voluptatum aliquam inventore! Harum quia quod accusamus fuga eum cum atque nemo id
               ratione. Ducimus reprehenderit ipsam sequi odit quo?</span>
           </p>
