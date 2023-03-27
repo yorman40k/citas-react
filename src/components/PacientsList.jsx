ee69fbbeda30cea08c38ccb0d5271c7d7825695a
yorman <yorman40k@gmail.com>
2023-03-27 15:41:20 -0500

    refactor: new way to control the content in the pacientList component

--- src/components/PacientsList.jsx
+++ src/components/PacientsList.jsx
@@ -3,25 +3,47 @@ import Pacient from "./Pacient"
 function PacientsList({ pacientes }) {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-        <h2 className="font-black text-3xl text-center">
-          Listado de pacientes
-        </h2>
 
-        <p className="text-lg mt-5 text-center mb-10">
-          Administra tus y {''}
-          <span className="text-indigo-600 font-bold">
-            Pacientes y Citas
-          </span>
-        </p>
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            Listado de pacientes
+          </h2>
 
-        {
-          pacientes.map( paciente => (
-            <Pacient
-              key={paciente.id}
-              paciente={paciente}
-            />
-          ))
-        }
+          <p className="text-lg mt-5 text-center mb-10">
+            Administra tus y {''}
+            <span className="text-indigo-600 font-bold">
+              Pacientes y Citas
+            </span>
+          </p>
+
+          {
+            pacientes.map( paciente => (
+              <Pacient
+                key={paciente.id}
+                paciente={paciente}
+              />
+            ))
+          }
+        </>
+      ) : (
+
+        <>
+          <h2 className="font-black text-3xl text-center">
+            No hay pacientes
+          </h2>
+
+          <p className="text-lg mt-5 text-center mb-10">
+            Comienza agregando pacientes y {''}
+            <span className="text-indigo-600 font-bold">
+              apareceran en este lugar
+            </span>
+          </p>
+        </>        
+      )
+      
+      }
+        
           
     </div>
   )
